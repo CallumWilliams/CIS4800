28ce0e8fdb0baf5413827a53d0ef79b26dfb0aa8
Callum Williams <callum.a.williams@gmail.com>
2018-01-18 15:03:53 -0500

    DrawImage

---
+++ CIS4800/CIS4800/DrawImage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+
+namespace CIS4800 {
+
+	public class DrawImage {
+
+		private int width;
+		private int height;
+		private Bitmap bmp;
+
+		/*
+		 * public DrawImage()
+		 * Parameters: m - dimensions of generated image
+		 * Initializes DrawImage object
+		 * */
+		public DrawImage (int m) {
+
+			this.width = m;
+			this.height = m;
+			this.bmp = new Bitmap (width, height);
+
+		}
+
+		/*
+		 * public void DrawPixelAt()
+		 * Parameters: x - horizontal cooordinate on image
+		 * 			   y - vertical coordinate on image
+		 * 			   r - color of the pixel
+		 * Sets pixel to be drawn at coordinates (x, y)
+		 * */
+		public void DrawPixelAt(int x, int y, Color r) {
+
+			this.bmp.SetPixel (x, y, r);
+
+		}
+
+		/*
+		 * public void SaveImage()
+		 * Parameters: none
+		 * Exports the generated bitmap to a .png file in root directory
+		 * */
+		public void SaveImage() {
+
+			this.bmp.Save ("../../../output/image.png");
+
+		}
+
+	}
+}
+
--- CIS4800/CIS4800/Program.cs
+++ CIS4800/CIS4800/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Drawing;
 
-namespace CIS4800
-{
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+namespace CIS4800 {
+	
+	class MainClass {
+		
+		public static void Main (string[] args) {
+
+			DrawImage img = new DrawImage (10);
+			img.DrawPixelAt (3, 3, Color.Red);
+			img.DrawPixelAt (6, 6, Color.Blue);
+			img.SaveImage ();
+
 		}
 	}
 }
