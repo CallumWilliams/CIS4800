617e0cca35d438c1521e302ddd32f0eea7104da6
Callum Williams <callum.a.williams@gmail.com>
2018-01-31 14:13:59 -0500

    Triangle Mesh Cube

--- CIS4800/CIS4800/Cube.cs
+++ CIS4800/CIS4800/Cube.cs
@@ -7,16 +7,12 @@ namespace CIS4800 {
 
 		float origin;
 		float length;
-		float width;
-		float height;
 
 		ArrayList edges;
 
 		public Cube (float len, float orig, MeshType m, int n) {
 
 			length = len;
-			width = len;
-			height = len;
 			origin = orig;
 
 			if (m == MeshType.Polygon)
@@ -34,14 +30,14 @@ namespace CIS4800 {
 			float z = origin + (length / 2);
 			//top surface
 			Vertex v1 = new Vertex (x, y, z);
-			Vertex v2 = new Vertex (x, y, z - width);
+			Vertex v2 = new Vertex (x, y, z - length);
 			ae.Add (new Edge (v1, v2));
 
 			v2 = new Vertex (x - length, y, z);
 			ae.Add (new Edge (v1, v2));
 
-			v1 = new Vertex (x - length, y, z - width);
-			v2 = new Vertex (x, y, z - width);
+			v1 = new Vertex (x - length, y, z - length);
+			v2 = new Vertex (x, y, z - length);
 			ae.Add (new Edge (v1, v2));
 
 			v2 = new Vertex (x - length, y, z);
@@ -49,34 +45,34 @@ namespace CIS4800 {
 
 			//side surfaces
 			v1 = new Vertex(x, y, z);
-			v2 = new Vertex (x, y - height, z);
+			v2 = new Vertex (x, y - length, z);
 			ae.Add (new Edge (v1, v2));
 
-			v1 = new Vertex (x, y, z - width);
-			v2 = new Vertex (x, y - length, z - width);
+			v1 = new Vertex (x, y, z - length);
+			v2 = new Vertex (x, y - length, z - length);
 			ae.Add (new Edge (v1, v2));
 
 			v1 = new Vertex (x - length, y, z);
-			v2 = new Vertex (x - length, y - height, z);
+			v2 = new Vertex (x - length, y - length, z);
 			ae.Add (new Edge (v1, v2));
 
-			v1 = new Vertex (x - length, y, z - width);
-			v2 = new Vertex (x - length, y - height, z - width);
+			v1 = new Vertex (x - length, y, z - length);
+			v2 = new Vertex (x - length, y - length, z - length);
 			ae.Add (new Edge (v1, v2));
 
 			//bottom surface
-			v1 = new Vertex(x, y - height, z);
-			v2 = new Vertex (x, y - height, z - length);
+			v1 = new Vertex(x, y - length, z);
+			v2 = new Vertex (x, y - length, z - length);
 			ae.Add (new Edge (v1, v2));
 
-			v2 = new Vertex (x - length, y - height, z);
+			v2 = new Vertex (x - length, y - length, z);
 			ae.Add (new Edge (v1, v2));
 
-			v1 = new Vertex (x - length, y - height, z - width);
-			v2 = new Vertex (x, y - height, z - length);
+			v1 = new Vertex (x - length, y - length, z - length);
+			v2 = new Vertex (x, y - length, z - length);
 			ae.Add (new Edge (v1, v2));
 
-			v2 = new Vertex (x - length, y - height, z);
+			v2 = new Vertex (x - length, y - length, z);
 			ae.Add (new Edge (v1, v2));
 
 			return ae;
@@ -86,6 +82,59 @@ namespace CIS4800 {
 		private ArrayList setupEdges_Triangle(int n) {
 
 			ArrayList ae = new ArrayList ();
+			float[] const_dimen = { origin - (length / 2), origin + (length / 2) };
+
+			for (int i = 0; i <= n; i++) {
+				for (int j = 0; j <= n; j++) {
+					for (int k = 0; k < const_dimen.Length; k++) {
+						double icoord = (double)((double)(length / n * i) - (length/2));
+						double jcoord = (double)((double)(length / n * j) - (length/2));
+						//Front and back
+						Vertex v1 = new Vertex((double)icoord, (double)jcoord, const_dimen[k]);
+						Vertex v2 = new Vertex ((double)icoord + (length / n), (double)jcoord, const_dimen [k]);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex((double)icoord, (double)jcoord + (length / n), const_dimen[k]);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex ((double)icoord + (length / n), (double)jcoord + (length / n), const_dimen [k]);
+						ae.Add (new Edge (v1, v2));
+						v1 = new Vertex ((double)icoord + (length / n), (double)jcoord + (length / n), const_dimen [k]);
+						v2 = new Vertex ((double)icoord + (length / n), (double)jcoord, const_dimen [k]);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex ((double)icoord, (double)jcoord + (length / n), const_dimen [k]);
+						ae.Add (new Edge (v1, v2));
+
+						//Top and bottom
+						v1 = new Vertex((double)icoord, const_dimen[k], (double)jcoord);
+						v2 = new Vertex ((double)icoord + (length / n), const_dimen [k], (double)jcoord);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex ((double)icoord, const_dimen [k], (double)jcoord + (length / n));
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex ((double)icoord + (length / n), const_dimen [k], (double)jcoord + (length / 2));
+						ae.Add (new Edge (v1, v2));
+						v1 = new Vertex ((double)icoord + (length / n), const_dimen [k], (double)jcoord + (length / n));
+						v2 = new Vertex ((double)icoord + (length / n), const_dimen [k], (double)jcoord);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex ((double)icoord, const_dimen [k], (double)jcoord + (length / n));
+						ae.Add (new Edge (v1, v2));
+
+						//Sides
+						v1 = new Vertex(const_dimen[k], (double)icoord, (double)jcoord);
+						v2 = new Vertex (const_dimen [k], (double)icoord + (length / n), (double)jcoord);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex (const_dimen [k], (double)icoord, (double)jcoord + (length / n));
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex (const_dimen [k], (double)icoord + (length / n), (double)jcoord + (length / n));
+						ae.Add (new Edge (v1, v2));
+						v1 = new Vertex (const_dimen [k], (double)icoord + (length / n), (double)jcoord + (length / n));
+						v2 = new Vertex (const_dimen [k], (double)icoord + (length / n), (double)jcoord);
+						ae.Add (new Edge (v1, v2));
+						v2 = new Vertex (const_dimen [k], (double)icoord, (double)jcoord + (length / n));
+						ae.Add (new Edge (v1, v2));
+
+					}
+				}
+			}
+
 			return ae;
 
 		}
--- CIS4800/CIS4800/Program.cs
+++ CIS4800/CIS4800/Program.cs
@@ -19,7 +19,7 @@ namespace CIS4800 {
 			//Vertex v2 = new Vertex (1, 1, -1);
 			//GraphicsMath.RasterizeEdge (new Edge (v1, v2), ref img);
 
-			Cube c = new Cube (1, 0, MeshType.Polygon, resolution);
+			Cube c = new Cube (1, 0, MeshType.Triangle, resolution);
 			c.Draw (ref img);
 			//GraphicsMath.DrawCube (ref img);
 			//GraphicsMath.DrawCube (ref img, MeshType.Triangle, resolution);
