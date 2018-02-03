d92bb2d3935582856bd2f038d9a77ea0bbf29b57
Callum Williams <callum.a.williams@gmail.com>
2018-02-03 11:11:41 -0500

    Cylinder Polygon

--- CIS4800/CIS4800/Cylinder.cs
+++ CIS4800/CIS4800/Cylinder.cs
@@ -7,7 +7,7 @@ namespace CIS4800 {
 
 		private const int MAX_RESOLUTION = 20;
 
-		Vertex origin;
+		Vertex origin;//"origin" in this shape is the top-centre of the 
 		double radius;
 		double height;
 		int resolution;
@@ -36,16 +36,29 @@ namespace CIS4800 {
 			for (int i = 0; i <= this.resolution; i++) {
 
 				double angle = 2 * Math.PI / resolution * i;
+				double nextAngle = 2 * Math.PI / resolution * (i + 1);
 
-				Vertex v1 = new Vertex (Math.Cos (angle), Math.Sin (angle), -1);
+				Vertex v1 = new Vertex (Math.Cos (angle), origin.getY (), Math.Sin (angle));
 				Vertex v2;
+
+				//top
 				if (i == resolution) {
-					v2 = new Vertex (Math.Cos (0), Math.Sin (0), -1);
+					v2 = new Vertex (Math.Cos (0), origin.getY (), Math.Sin (0));
 				} else {
-					double nextAngle = 2 * Math.PI / resolution * (i + 1);
-					v2 = new Vertex (Math.Cos (nextAngle), Math.Sin (nextAngle), -1);
+					v2 = new Vertex (Math.Cos (nextAngle), origin.getY (), Math.Sin (nextAngle));
 				}
 				ae.Add (new Edge (v1, v2));
+				//bottom
+				v1 = new Vertex(Math.Cos (angle), origin.getY () - height, Math.Sin (angle));
+				Vertex v3;
+				if (i == resolution) {
+					v3 = new Vertex (Math.Cos (0), origin.getY () - height, Math.Sin (0));
+				} else {
+					v3 = new Vertex (Math.Cos (nextAngle), origin.getY () - height, Math.Sin (nextAngle));
+				}
+				ae.Add (new Edge (v1, v3));
+				//add sides
+				ae.Add (new Edge (v2, v3));
 
 			}
 
